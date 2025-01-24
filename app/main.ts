de94a4952681075f4be69eaadf60432f9a6ed4dd
AdityaP700 <126982848+AdityaP700@users.noreply.github.com>
2025-01-24 17:05:20 +0530

    [any message]

--- app/main.ts
+++ app/main.ts
@@ -4,31 +4,24 @@ import * as net from "net";
 const server = net.createServer((socket) => {
   console.log("New Connection Established");
 
-  // Handle the 'data' event (when a client sends a request)
   socket.on("data", (data) => {
     console.log(`Received data: ${data}`);
   });
 
-  // Handle the 'close' event (when a client disconnects)
-  socket.on("close", () => {
-    console.log("Connection closed");
-  });
+  socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
+  socket.end();
 
-  // Handle errors
   socket.on("error", (err) => {
     console.log(`Socket error: ${err.message}`);
   });
 });
 
-// Log a message to confirm the server is running
 console.log("Logs from your program will appear here!");
 
-// Start the server on port 4221
 server.listen(4221, "localhost", () => {
   console.log("TCP server is listening on Port 4221");
 });
 
-// Handle server-level errors
 server.on("error", (err) => {
   console.log(`Server error: ${err.message}`);
 });
