1b62e3f79516c8ebce6eef735ea33dc8103a004b
AdityaP700 <126982848+AdityaP700@users.noreply.github.com>
2025-01-24 18:20:49 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -1,29 +1,28 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var net = require("net");
 // Create the server
 var server = net.createServer(function (socket) {
     console.log("New Connection Established");
-    // Handle the 'data' event (when a client sends a request)
     socket.on("data", function (data) {
-        console.log("Received data: ".concat(data));
+        console.log("Received data: ".concat(data.toString()));
+        var request = data.toString();
+        var urlPath = request.split(" ")[1];
+        // Determine the response based on the URL path
+        var response = urlPath === '/'
+            ? 'HTTP/1.1 200 OK\r\n\r\n'
+            : 'HTTP/1.1 404 Not Found\r\n\r\n';
+        socket.write(response); // Send the response
+        socket.end(); // End the connection
     });
-    // Handle the 'close' event (when a client disconnects)
-    socket.on("close", function () {
-        console.log("Connection closed");
-    });
-    // Handle errors
     socket.on("error", function (err) {
         console.log("Socket error: ".concat(err.message));
     });
 });
-// Log a message to confirm the server is running
 console.log("Logs from your program will appear here!");
-// Start the server on port 4221
 server.listen(4221, "localhost", function () {
     console.log("TCP server is listening on Port 4221");
 });
-// Handle server-level errors
 server.on("error", function (err) {
     console.log("Server error: ".concat(err.message));
 });
